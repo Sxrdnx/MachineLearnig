4035cb6dbae8363f68094e616043f56443b1cd7b
Sxrdnx <serdna.alejandro@gmail.com>
2020-07-23 17:24:54 -0500

    escalado de datos

--- dataPreprocessing/dataProcessing.py
+++ dataPreprocessing/dataProcessing.py
@@ -36,4 +36,9 @@ x=np.array(ct.fit_transform(x),dtype=np.float)
 #dividir data set in trainin y test
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
+#escalando datos 
+from sklearn .preprocessing import StandardScaler
+sc_x=StandardScaler()
+x_train=sc_x.fit_transform(x_train)
+x_test=sc_x.fit_transform(x_test)
 
