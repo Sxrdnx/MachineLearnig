f3e0d5cd8eea4119767ae37ac40e7897a00e784d
Sxrdnx <serdna.alejandro@gmail.com>
2020-07-27 19:30:38 -0500

    Escalado_&_divicion_R

--- dataPreprocessing/dataPreprocessing.R
+++ dataPreprocessing/dataPreprocessing.R
@@ -2,12 +2,27 @@
 #importamos dataset
 dataset=read.csv("Data.csv")
 #tartamiento de datos faltantes
+
 dataset$Age=ifelse(is.na(dataset$Age),
                    ave(dataset$Age, FUN = function(x) mean(x,na.rm=TRUE))
                    ,dataset$Age)
 dataset$Salary=ifelse(is.na(dataset$Salary),
-                   ave(dataset$Salary, FUN = function(x) mean(x,na.rm=TRUE))
-                   ,dataset$Salary)
+                      ave(dataset$Salary, FUN = function(x) mean(x,na.rm=TRUE))
+                      ,dataset$Salary)
 
-
+#Datos categoricos
+dataset$Country=factor(dataset$Country,
+                       levels=c("France","Spain","Germany"),
+                       labels = c(1,2,3))
+dataset$Purchased=factor(dataset$Purchased,
+                         levels=c("No","Yes"),
+                         labels = c(0,1))
+#Divicion del data setn en entrenamiento y test
+set.seed(123)
+split=sample.split(dataset$Purchased,SplitRatio = 0.8)
+training_set=subset(dataset,split==TRUE)
+testing_set=subset(dataset,split==FALSE)
+#Escalado de datos
+training_set[,2:3]=scale(training_set[,2:3])
+testing_set[,2:3]=scale(testing_set[,2:3])
 
--- dataPreprocessing/dataProcessing.py
+++ dataPreprocessing/dataProcessing.py
@@ -39,6 +39,6 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
 #escalando datos 
 from sklearn .preprocessing import StandardScaler
 sc_x=StandardScaler()
-x_train=sc_x.fit_transform(x_train)
 x_test=sc_x.fit_transform(x_test)
+x_train=sc_x.fit_transform(x_train)
 
