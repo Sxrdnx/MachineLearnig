9102ee2cc14a32cbba88ee08c3c6f71e6e3282a7
Sxrdnx <serdna.alejandro@gmail.com>
2020-08-28 10:01:27 -0500

    regMult_tratamiento_Data

---
+++ regresionMultiple/multiple_lineal_regression.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import numpy as np 
+dataset=pd.read_csv("50_Startups.csv")
+x=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,4].values
+#manejo de datos categoricos(var-dummy) sin usar labelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+ct=ColumnTransformer([("one_hot_encoder",OneHotEncoder(categories="auto"),[3])],
+                     remainder="passthrough"
+                     )
+x=np.array(ct.fit_transform(x),dtype=np.float)
+#eliminamos la primera var dummy
+x=x[:,1:]
+#divicion set de entrenamiento y testing
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
+
+
+
