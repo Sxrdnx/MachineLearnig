9163b99ea07ce4dd1e372cf5d3a6e08e16286453
Sxrdnx <serdna.alejandro@gmail.com>
2020-09-01 14:14:47 -0500

    ajuste y prediccion

--- regresionMultiple/multiple_lineal_regression.py
+++ regresionMultiple/multiple_lineal_regression.py
@@ -16,6 +16,10 @@ x=x[:,1:]
 #divicion set de entrenamiento y testing
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
-
-
+#ajustar elmodelo con el conjunto de entrenamiento
+from sklearn.linear_model import LinearRegression
+regression=LinearRegression()
+regression.fit(x_train,y_train)
+#Prediccion en los datos de testing 
+y_pred=regression.predict(x_test)
 
